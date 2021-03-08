31543d19281226867ddf2367e956115314496d10
Callum1scott <61849378+Callum1scott@users.noreply.github.com>
2021-03-08 15:58:20 -0500

    CallumQuiz3

---
+++ Assets/Items.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Items : MonoBehaviour
+{
+	public string Name;
+	public int Calories;
+	public int Karmas;
+	public bool Alive;
+	public string Message;
+
+	private void OnMouseDown()
+	{
+		UseItem();
+	}
+
+	public virtual void UseItem()
+
+	{
+		
+
+
+		Debug.Log("Clicked");
+
+	}
+}
